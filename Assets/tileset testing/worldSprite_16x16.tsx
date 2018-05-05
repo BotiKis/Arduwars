8d718d94d67e257331bc5e4531e6c801bc2850ee
Botond Kis <botond.kis@gmail.com>
2018-05-05 13:27:44 +0200

    updated worldsprite

--- Assets/tileset testing/worldSprite_16x16.tsx
+++ Assets/tileset testing/worldSprite_16x16.tsx
@@ -2,19 +2,21 @@
 <tileset name="worldSprite_16x16" tilewidth="16" tileheight="16" tilecount="34" columns="1">
  <image source="../worldSprite_16x16.png" width="16" height="544"/>
  <terraintypes>
-  <terrain name="Land" tile="1"/>
+  <terrain name="Land" tile="14"/>
+  <terrain name="Water" tile="0"/>
  </terraintypes>
- <tile id="1" terrain="0,0,0,0"/>
- <tile id="2" terrain="0,0,0,"/>
- <tile id="3" terrain="0,,0,"/>
- <tile id="4" terrain="0,,0,0"/>
- <tile id="5" terrain=",,0,0"/>
- <tile id="6" terrain="0,0,,"/>
- <tile id="7" terrain="0,0,,0"/>
- <tile id="8" terrain=",0,,0"/>
- <tile id="9" terrain=",0,0,0"/>
- <tile id="10" terrain="0,,,"/>
- <tile id="11" terrain=",,0,"/>
- <tile id="12" terrain=",,,0"/>
- <tile id="13" terrain=",0,,"/>
+ <tile id="0" terrain="1,1,1,1"/>
+ <tile id="2" terrain="0,0,0,1"/>
+ <tile id="3" terrain="0,1,0,1"/>
+ <tile id="4" terrain="0,1,0,0"/>
+ <tile id="5" terrain="1,1,0,0"/>
+ <tile id="6" terrain="0,0,1,1"/>
+ <tile id="7" terrain="0,0,1,0"/>
+ <tile id="8" terrain="1,0,1,0"/>
+ <tile id="9" terrain="1,0,0,0"/>
+ <tile id="10" terrain="0,1,1,1"/>
+ <tile id="11" terrain="1,1,0,1"/>
+ <tile id="12" terrain="1,1,1,0"/>
+ <tile id="13" terrain="1,0,1,1"/>
+ <tile id="14" terrain="0,0,0,0"/>
 </tileset>
