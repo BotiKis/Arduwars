1468f6567188164c5dc9a77e342ad0542f3d151e
Botond Kis <botond.kis@gmail.com>
2018-05-06 22:15:24 +0200

    added dataclasses

---
+++ Arduwars/src/DataClasses.cpp
@@ -0,0 +1,228 @@
+#include "DataClasses.h"
+
+Unit::Unit(){
+  // Save the provided Type
+  unitType = static_cast<uint8_t>(UnitType::Soldier);
+
+  // By default set every attribute to 0
+  healthPoints = 0;
+  attackPower = 0;
+  moveDistance = 0;
+  attackRange = 0;
+}
+
+Unit::Unit(UnitType type){
+  // Save the provided Type
+  unitType = static_cast<uint8_t>(type);
+
+  // Every unit has default 10 HP
+  healthPoints = 10;
+
+  // set unused stuff to 0
+  others = 0;
+
+  // Switch through types
+  switch (type) {
+    default:
+    // Infantry
+    case UnitType::Soldier:{
+      attackPower  = 1; // MAX 7
+      defense      = 0; // MAX 3
+      moveDistance = 3; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    case UnitType::Mech:{
+      attackPower  = 3; // MAX 7
+      defense      = 0; // MAX 3
+      moveDistance = 2; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    case UnitType::SpecOps:{
+      attackPower  = 3; // MAX 7
+      defense      = 0; // MAX 3
+      moveDistance = 5; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    // Vehicles
+    case UnitType::Recon:{
+      attackPower  = 3; // MAX 7
+      defense      = 1; // MAX 3
+      moveDistance = 6; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    case UnitType::Assist:{
+      attackPower  = 0; // MAX 7
+      defense      = 1; // MAX 3
+      moveDistance = 7; // MAX 7
+      attackRange  = 0; // MAX 7
+      break;
+    };
+    case UnitType::Tank:{
+      attackPower  = 5; // MAX 7
+      defense      = 2; // MAX 3
+      moveDistance = 4; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    case UnitType::BigTank:{
+      attackPower  = 7; // MAX 7
+      defense      = 3; // MAX 3
+      moveDistance = 2; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    case UnitType::Artillery:{
+      attackPower  = 4; // MAX 7
+      defense      = 1; // MAX 3
+      moveDistance = 3; // MAX 7
+      attackRange  = 5; // MAX 7
+      break;
+    };
+    case UnitType::Rocket:{
+      attackPower  = 5; // MAX 7
+      defense      = 1; // MAX 3
+      moveDistance = 3; // MAX 7
+      attackRange  = 4; // MAX 7
+      break;
+    };
+    case UnitType::Missiles:{
+      attackPower  = 6; // MAX 7
+      defense      = 0; // MAX 3
+      moveDistance = 4; // MAX 7
+      attackRange  = 6; // MAX 7
+      break;
+    };
+    // Planes
+    case UnitType::Heli:{
+      attackPower  = 3; // MAX 7
+      defense      = 1; // MAX 3
+      moveDistance = 5; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    case UnitType::Fighter:{
+      attackPower  = 4; // MAX 7
+      defense      = 2; // MAX 3
+      moveDistance = 7; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    case UnitType::Bomber:{
+      attackPower  = 7; // MAX 7
+      defense      = 3; // MAX 3
+      moveDistance = 3; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    // Ships
+    case UnitType::Cruiser:{
+      attackPower  = 3; // MAX 7
+      defense      = 1; // MAX 3
+      moveDistance = 4; // MAX 7
+      attackRange  = 1; // MAX 7
+      break;
+    };
+    case UnitType::Battleship:{
+      attackPower  = 5; // MAX 7
+      defense      = 3; // MAX 3
+      moveDistance = 5; // MAX 7
+      attackRange  = 4; // MAX 7
+      break;
+    };
+    case UnitType::Transportship:{
+      attackPower  = 0; // MAX 7
+      defense      = 1; // MAX 3
+      moveDistance = 7; // MAX 7
+      attackRange  = 0; // MAX 7
+      break;
+    };
+
+  }
+}
+
+Building::Building(){
+  // Save the provided Type
+  buildingType = static_cast<uint8_t>(BuildingType::None);
+
+    // By default set every attribute to 0
+  healthPoints = 0;
+  movePenalty = 0;
+  defenseBonus = 0;
+  others = 0;
+}
+
+Building::Building(BuildingType type){
+  // Save the provided Type
+  buildingType = static_cast<uint8_t>(type);
+
+  // Every Building has default 10 HP
+  healthPoints = 10;
+
+  // set unused stuff to 0
+  others = 0;
+
+  // Switch through types
+  switch (type) {
+    default:
+    case BuildingType::Hill:{
+      movePenalty   = 1;  // MAX 3
+      defenseBonus  = 2;  // MAX 3
+      break;
+    };
+    case BuildingType::Mountain:{
+      movePenalty   = 3;  // MAX 3
+      defenseBonus  = 2;  // MAX 3
+      break;
+    };
+    case BuildingType::Forest:{
+      movePenalty   = 1;  // MAX 3
+      defenseBonus  = 1;  // MAX 3
+      break;
+    };
+    case BuildingType::City:{
+      movePenalty   = 1;  // MAX 3
+      defenseBonus  = 1;  // MAX 3
+      break;
+    };
+    case BuildingType::Factory:{
+      movePenalty   = 0;  // MAX 3
+      defenseBonus  = 1;  // MAX 3
+      break;
+    };
+    case BuildingType::Airport:{
+      movePenalty   = 0;  // MAX 3
+      defenseBonus  = 1;  // MAX 3
+      break;
+    };
+    case BuildingType::Shipyard:{
+      movePenalty   = 0;  // MAX 3
+      defenseBonus  = 1;  // MAX 3
+      break;
+    };
+    case BuildingType::ScienceFacility:{
+      movePenalty   = 0;  // MAX 3
+      defenseBonus  = 1;  // MAX 3
+      break;
+    };
+    case BuildingType::HQPlayer1:{
+      movePenalty   = 0;  // MAX 3
+      defenseBonus  = 1;  // MAX 3
+      healthPoints  = 15; // HQ is only building with 15 HP
+      break;
+    };
+    case BuildingType::HQPlayer2:{
+      movePenalty   = 0;  // MAX 3
+      defenseBonus  = 1;  // MAX 3
+      healthPoints  = 15; // HQ is only building with 15 HP
+      break;
+    };
+  }
+}
+
+Player::Player(){
+  money = 20;
+}
--- Arduwars/src/DataClasses.h
+++ Arduwars/src/DataClasses.h
@@ -9,6 +9,51 @@
 // Data Classes are a place where only Information is stored.
 // There will be no visual nor game logic elements of the game.
 
+// This enum defines the possible Units
+// There are only 16 in total.
+enum class UnitType : uint8_t {
+  // Infantry
+  Soldier = 0,
+  Mech,
+  SpecOps,
+
+  // Vehicles
+  Recon,
+  Assist,      // Heals all adjacent Units at the start of the turn and can carry Infantry
+  Tank,
+  BigTank,
+  Artillery,
+  Rocket,       // Can only shoot Planes and Ships
+  Missiles,
+
+  // Planes
+  Heli,
+  Fighter,      // Can Only fight Planes
+  Bomber,
+
+  // Ships    // Can only go over water
+  Cruiser,    // Can only fight Ships
+  Battleship,
+  Transportship // Can transport Infantry
+};
+
+// This enum defines the possible buildings.
+// A Building can be the HQ, a city or even a Mountain or Forests.
+// There are only 16 in total.
+enum class BuildingType : uint8_t {
+  None = 0,
+  Hill,     // Can only be moved to by Infantry
+  Mountain, // Can only be moved to by Infantry
+  Forest,
+  City,
+  Factory,
+  Airport,
+  Shipyard,
+  ScienceFacility,
+  HQPlayer1,
+  HQPlayer2
+};
+
 
 // This class defines a Unit like Soldiers or Tanks
 class Unit{
@@ -35,32 +80,55 @@ public:
   // architectures/platforms than the Arduboy.
 
   uint8_t unitType    :4; // We can have 16 different Units going from 0-15
+                          // Also we store it as a uint8_t instead of a UnitType,
+                          // because we save some valuable ram if we use a Bitfield.
+                          // Later we typecast safely between these two types.
   uint8_t healthPoints:4; // A unit can have max 15 health.
-  uint8_t attackPower :2; // Can have max 3 attackPower.
+  uint8_t attackPower :3; // Can have max 7 attackPower.
   uint8_t defense     :2; // Can have max 3 defense.
   uint8_t moveDistance:3; // Can move in a radius of max 7 tiles.
   uint8_t attackRange :3; // Can attack in a radius of max 7 tiles.
-  uint8_t others      :6; // reserved for future use.
+  uint8_t others      :5; // reserved for future use.
   // 3 Bytes in total.
+
+  // Constructor
+  // Default constructor initializes an all 0 filled UnitType::Soldier Unit.
+  Unit();
+  // Custom Constructor is used to initialize with default content. See DataClass.cpp for the implementation.
+  Unit(UnitType atype);
 };
 
 // This class defines a Building like the HQ or Cities.
 // Mountains, Forests and other things on the Map also count as buildings.
 class Building{
+public:
   uint8_t buildingType  :4; // We can have 16 different buildings
+                            // Also we store it as a uint8_t instead of a BuildingType,
+                            // because we save some valuable ram if we use a Bitfield.
+                            // Later we typecast safely between these two types.
   uint8_t healthPoints  :4; // A building can have max 15 health.
   uint8_t movePenalty   :2; // A building can give a moving Unit a movement penalty up to 3.
   uint8_t defenseBonus  :2; // A building can give a stationed Unit a defense bonus up to 3.
   uint8_t others        :4; // reserved for future use.
   // 2 Bytes in total.
+
+  // Constructor
+  // Default constructor initializes an all 0 filled BuildingType::None Building.
+  Building();
+  // Custom Constructor is used to initialize with default content. See DataClass.cpp for the implementation.
+  Building(BuildingType type);
 };
 
 // This class defines a Player which has Units, Buildings, Money and other Information.
 class Player{
+public:
   uint8_t money;
-  Array<Unit, 24> units;          // 24 Units make 72 Bytes + some overhead.
-  Array<Building, 16> buildings;  // 16 Buildings makes 32 Bytes + some overhead.
-  // Approximately 110 bytes.
+  Array<Unit, 24> units;          // 24 Units make approximately 72 Bytes.
+  Array<Building, 16> buildings;  // 16 Buildings approximately makes 32 Bytes.
+  // Approximately 106 bytes in total.
+
+  // Constructor is used to initialize with default content. See DataClass.cpp for the implementation.
+  Player();
 };
 
 #endif
