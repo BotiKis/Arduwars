e7019c099c42d28c75124f981f147dc3ac8196f2
Botond Kis <botond.kis@gmail.com>
2018-05-20 17:05:41 +0200

    added magnitude function to pointmath

--- Arduwars/src/PointMath.h
+++ Arduwars/src/PointMath.h
@@ -3,29 +3,36 @@
 
 #include <Arduboy2.h>
 
-using Vector2D = Point;
+  using Vector2D = Point;
 
-// Overload == perator for comparisson
-static inline bool operator==(const Vector2D &lhs, const Vector2D &rhs){
-    return (lhs.x == rhs.x && lhs.y == rhs.y);
-}
+  // Overload == perator for comparisson
+  static inline bool operator==(const Vector2D &lhs, const Vector2D &rhs){
+      return (lhs.x == rhs.x && lhs.y == rhs.y);
+  }
 
-//Other maths
-static inline Vector2D operator+(const Vector2D &lhs, const Vector2D &rhs){
-    return {lhs.x+rhs.x, lhs.y+rhs.y};
-}
+  //Other maths
+  static inline Vector2D operator+(const Vector2D &lhs, const Vector2D &rhs){
+      return {lhs.x+rhs.x, lhs.y+rhs.y};
+  }
 
-static inline Vector2D operator-(const Vector2D &lhs, const Vector2D &rhs){
-    return {lhs.x-rhs.x, lhs.y-rhs.y};
-}
+  static inline Vector2D operator-(const Vector2D &lhs, const Vector2D &rhs){
+      return {lhs.x-rhs.x, lhs.y-rhs.y};
+  }
 
-// multyply with an skalar
-static inline Vector2D operator*(const Vector2D &lhs, const int16_t &rhs){
-    return {lhs.x*rhs, lhs.y*rhs};
-}
+  // multyply with an skalar
+  static inline Vector2D operator*(const Vector2D &lhs, const int16_t &rhs){
+      return {lhs.x*rhs, lhs.y*rhs};
+  }
 
-static inline Vector2D operator/(const Vector2D &lhs, const int16_t &rhs){
-    return {lhs.x/rhs, lhs.y/rhs};
+  static inline Vector2D operator/(const Vector2D &lhs, const int16_t &rhs){
+      return {lhs.x/rhs, lhs.y/rhs};
+  }
+
+
+namespace PointMath{
+  static inline uint16_t magnitude(const Vector2D &vector){
+      return sqrt(vector.x*vector.x + vector.y*vector.y);
+  }
 }
 
 #endif
